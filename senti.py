792f2777d6852951248cc62c0cbb93ee33318d55
aarnavpbhat <109448062+aarnavpbhat@users.noreply.github.com>
2023-03-18 19:08:19 -0700

    Input is given

--- senti.py
+++ senti.py
@@ -1,10 +1,13 @@
+from flask import Flask, render_template, request
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 import string
 import nltk
+
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
 
+app = Flask(__name__)
 
 def preprocess_text(text):
     stop_words = set(stopwords.words('english'))
@@ -22,16 +25,16 @@ def analyze_sentiment(text):
         return 'Negative'
     else:
         return 'Neutral'
-    
-text1 = "I had a great day at the beach today. The weather was perfect and I had a lot of fun."
-text2 = "I'm so disappointed with the service at this restaurant. The food was terrible and the staff was rude."
-text3 = "I went to the grocery store today and bought some milk and bread."
 
-sentiment1 = analyze_sentiment(text1)
-sentiment2 = analyze_sentiment(text2)
-sentiment3 = analyze_sentiment(text3)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-print("Text 1 sentiment:", sentiment1)
-print("Text 2 sentiment:", sentiment2)
-print("Text 3 sentiment:", sentiment3)
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form['text']
+    sentiment = analyze_sentiment(text)
+    return render_template('index.html', text=text, sentiment=sentiment)
 
+if __name__ == '__main__':
+    app.run(debug=True)
